82857ab5fc0463262228345c18b85c6cc155817e
Muhammad Irfan Zeerak <105989392+IrfanZeerak@users.noreply.github.com>
2025-04-20 23:01:49 +0500

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1swlXlfJcfAz2ueGqNfmMZF_7hOuNTIYG
 """
 
+pip install tensorflow
 
 import streamlit as st
 import numpy as np
